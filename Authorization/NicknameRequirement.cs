26674543a6bcff1ee199548f57a15c6ea5cb49d5
alwin-cherian <57612552+alwin-cherian@users.noreply.github.com>
2023-03-28 15:17:40 +0530

    Add files via upload

---
+++ Authorization/NicknameRequirement.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Week6_Identity_CRUD.Authorization
+{
+    public class NicknameRequirement : IAuthorizationRequirement
+    {
+        public string Name { get; set; }
+        public NicknameRequirement(string name)
+        {
+            Name = name;
+        }
+    }
+}
